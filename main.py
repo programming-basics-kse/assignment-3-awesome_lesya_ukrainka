48eeea0448f9a34f2fd9355a4aa57b15734e6389
tbaraniuk <baraniuktaras098@gmail.com>
2024-11-18 10:23:11 +0200

    add getting overall stats for countries

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 parser = argparse.ArgumentParser(
         prog="Awesome Lesya Ukrainka",
         description="Analyzing olympic athlets dataset"
@@ -8,7 +9,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('filename', help='name of input file')
 parser.add_argument('-medals', '--medals', nargs=2, help='Used for getting count of medals for each type of countries')
 parser.add_argument('-total', '--total', help='Used for getting total result for each country')
-parser.add_argument('-overall', '--overall', help="Used for getting highest medals and year for following countries")
+parser.add_argument('-overall', '--overall', nargs="*", help="Used for getting highest medals and year for following countries")
 parser.add_argument('-interactive', '--interactive', help="Used for getting statistic for current country")
 parser.add_argument('-output', '--output', nargs=1, help='name of output file')
 
@@ -41,20 +42,18 @@ def get_medals_count(filename: str, team: str, year: str) -> str:
                     res += f'{name} - {line_year} - {medal}' + '\n'
 
         if i == 10:
-            print('There wasn\'t an olympiad this year')
             res += 'There wasn\'t an olympiad this year \n'
-
-    print(f'Total gold: {gold}')
-    print(f'Total silver: {silver}')
-    print(f'total bronze: {bronze}')
 
     res += f'Total gold: {gold}\n' + f'Total silver: {silver}\n' + f'total bronze: {bronze}\n'
 
+    print(res)
     return res
 
 
-def get_medals_total(filename: str, year: str):
+def get_medals_total(filename: str, year: str) -> str:
     count_medals = {}
+    res = ''
+
     with open(filename) as file:
         for line in file:
             one_line = line.split('\t')
@@ -83,27 +82,69 @@ def get_medals_total(filename: str, year: str):
                     elif 'Bronze' in medal:
                         count_medals[line_country]['Bronze'] += 1
 
+    for country in count_medals:
+        res += f"{country}: {count_medals[country]['Gold']} - {count_medals[country]['Silver']} - {count_medals[country]['Bronze']} \n"
+
+    print(res)
+    return res
 
 
+def get_medals_overall(filename: str, countries: list[str]) -> str:
+    data = {}
+    res = ''
 
-    for countrie in count_medals:
-        print(f"{countrie}: {count_medals[countrie]['Gold']} - {count_medals[countrie]['Silver']} - {count_medals[countrie]['Bronze']}")
+    with open(filename) as file:
+        for line in file:
+            one_line = line.split('\t')
+            country = one_line[6]
 
+            if country not in countries:
+                continue
 
+            year = one_line[9]
+            medal = one_line[-1]
+
+            if country not in data:
+                data[country] = {}
+
+            if year not in data[country]:
+                data[country][year] = 0
+
+            if 'NA' not in medal:
+                data[country][year] += 1
+
+    for item in data.keys():
+        stats = data[item]
+        year = ''
+        medals = 0
+
+        for key, val in stats.items():
+            if val > medals:
+                year = key
+                medals = val
+
+        res += f"{item} - {year} - {medals} \n"
+
+    print(res)
+    return res
 
 
 def main():
     args = parser.parse_args()
 
     if args.medals:
-        res = get_medals_count('olympic_athlets.tsv', args.medals[0], args.medals[1])
+        res = get_medals_count(args.filename, args.medals[0], args.medals[1])
+
+    if args.total:
+       res = get_medals_total(args.filename, args.total)
+
+    if args.overall:
+        res = get_medals_overall(args.filename, args.overall)
 
     if args.output:
         with open(args.output[0], "w") as file:
             file.write(res)
 
-    if args.total:
-        get_medals_total('olympic_athlets.tsv', args.total)
 
 if __name__ == '__main__':
     main()
