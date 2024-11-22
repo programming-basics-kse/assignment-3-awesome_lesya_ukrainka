1bba863565fd2c53028f09b59425329c35a53e82
tbaraniuk <baraniuktaras098@gmail.com>
2024-11-22 11:19:20 +0200

    add total stats for task 4

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def get_medals_total(filename: str, year: str) -> str:
                 one_line = line.split('\t')
                 line_year = one_line[-6]
                 line_country = one_line[6]
-                medal = one_line[-1]
+                medal = one_line[-1].strip()
 
                 if year == line_year:
                     if line_country not in count_medals:
@@ -141,15 +141,79 @@ def get_medals_overall(filename: str, countries: list[str]) -> str:
 
 def  get_interactive_statistics(filename: str, country: str) -> str:
     try:
+        res = ''
+        first_participation = {}
+        total_medals_by_year = {}
+
         with open(filename) as file:
             for line in file:
                 one_line = line.split('\t')
                 team_code = one_line[7]
                 line_country = one_line[6]
-                if country != (team_code or line_country):
+                if country.lower() != team_code.lower() and country.lower() not in line_country.lower():
                     continue
+
+                medal = str(one_line[-1]).strip()
+                year = int(one_line[9])
+                city = one_line[11]
+
+                if 'year' not in first_participation:
+                    first_participation['year'] = year
+                    first_participation['city'] = city
+                elif year < first_participation['year']:
+                    first_participation['year'] = year
+                    first_participation['city'] = city
+
+                if 'NA' not in medal and medal:
+                    if year not in total_medals_by_year:
+                        total_medals_by_year[year] = {
+                            'count': 1,
+                            'gold': 1 if medal == 'Gold' else 0,
+                            'silver': 1 if medal == 'Silver' else 0,
+                            'bronze': 1 if medal == 'Bronze' else 0,
+                        }
+                    else:
+                        total_medals_by_year[year]['count'] += 1
+
+                        if medal == 'Gold':
+                            total_medals_by_year[year]['gold'] += 1
+                        elif medal == 'Silver':
+                            total_medals_by_year[year]['silver'] += 1
+                        elif medal == 'Bronze':
+                            total_medals_by_year[year]['bronze'] += 1
+
+        if 'year' in first_participation:
+            res += f'First participation: {first_participation['year']} in {first_participation['city']} \n'
+
+        if total_medals_by_year:
+            max_year, max_medals = max(total_medals_by_year.items(), key=lambda x: x[1]['count'])
+
+            if max_medals:
+                res += f'The most successful olympiad: in {max_year} ({max_medals['count']} medals) \n'
+
+            min_year, min_medals = min(total_medals_by_year.items(), key=lambda x: x[1]['count'])
+
+            if min_medals:
+                res += f'The most unsuccessful olympiad: in {min_year} ({min_medals['count']} medals) \n\n'
+
+            gold_medals = [item['gold'] for item in total_medals_by_year.values()]
+            silver_medals = [item['silver'] for item in total_medals_by_year.values()]
+            bronze_medals = [item['bronze'] for item in total_medals_by_year.values()]
+
+            avg_gold_medals = round(sum(gold_medals) / len(gold_medals), 1)
+            avg_silver_medals = round(sum(silver_medals) / len(silver_medals), 1)
+            avg_bronze_medals = round(sum(bronze_medals) / len(bronze_medals), 1)
+
+            res += f'Average of gold medals: {avg_gold_medals} \n'
+            res += f'Average of silver medals: {avg_silver_medals} \n'
+            res += f'Average of bronze medals: {avg_bronze_medals} \n'
+
+        print(res)
+        return res
+
     except Exception:
         print('Please, enter a valid dataset filename')
+        return ''
 
 
 def main():
@@ -170,14 +234,19 @@ def main():
             else:
                 one_country = get_interactive_statistics(args.filename, country)
                 res += one_country
-                while one_country == '':
+                while one_country == '' and country != 'exit':
                     country = input('No country that matches this, try again: ')
+
+                    if country == 'exit':
+                        return
+
                     one_country = get_interactive_statistics(args.filename, country)
                     res += one_country
 
     if args.output:
         with open(args.output[0], "w") as file:
             file.write(res)
+
 
 if __name__ == '__main__':
     main()
